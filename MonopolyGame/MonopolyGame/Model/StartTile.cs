53597252ec946dcb95aa0a991a102ecaf1ff2be7
tcholakov <tcholakov2@gmail.com>
2015-03-22 12:08:54 +0200

    Added StartTile.cs for real...

---
+++ MonopolyGame/MonopolyGame/Model/StartTile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyGame.Model
+{
+    class StartTile : Tile,ITile
+    {
+        public StartTile()
+            : base(0) 
+        { }
+
+
+
+        public void Action(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
