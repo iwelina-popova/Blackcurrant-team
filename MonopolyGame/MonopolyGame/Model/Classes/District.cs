aa279e07039218bf973d8f8f099d78dc71c0c656
iwelina-popova <iwelina.popova@gmail.com>
2015-03-25 15:11:13 +0200

    Some idea for District.cs

---
+++ MonopolyGame/MonopolyGame/Model/Classes/District.cs
@@ -0,0 +1,29 @@
+﻿namespace MonopolyGame.Model.Classes
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class District
+    {
+        public List<StreetTile> LoadDistrict(List<StreetTile> streets, StreetTile currentStreet)
+        {
+            List<StreetTile> district = new List<StreetTile>();
+
+            foreach (var street in streets)
+            {
+                if (street.Color == currentStreet.Color)
+                {
+                    district.Add(street);
+                }
+            }
+
+            return district;
+        }
+
+        public bool CheckDistrictOwner(List<StreetTile> district)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
